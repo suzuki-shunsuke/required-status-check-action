1c5d9280db36884276ea50c12709c080d904db28
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-03-05 09:42:30 +0900

    fix: add a log

--- src/run.ts
+++ src/run.ts
@@ -106,15 +106,17 @@ const getWorkflow = async (input: Input): Promise<Workflow> => {
 
   // reads or downloads the workflow file
   const octokit = github.getOctokit(input.githubToken);
+  core.info(`fetching workflow file ${workflowPath} (${input.workflowSHA}) from ${workflowOwner}/${workflowRepo}`);
   const resp = await octokit.rest.repos.getContent({
     owner: workflowOwner,
     repo: workflowRepo,
     path: workflowPath,
     ref: input.workflowSHA,
   });
+  resp.status
   const data = resp.data as { content?: string };
   if (data.content === undefined) {
-    throw new Error("workflow file is not a file");
+    throw new Error(`workflow file is not a file: (${resp.status}) ${JSON.stringify(resp.data)}`);
   }
   if (data.content === "") {
     throw new Error("workflow file is empty");
